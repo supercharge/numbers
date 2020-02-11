2bb20c388ba4e8416b7a8588858527c46a14224c
Marcus Poehls <marcus.poehls@gmail.com>
2020-02-11 05:55:07 +0100

    export num directly

--- src/index.js
+++ src/index.js
@@ -2,11 +2,5 @@
 
 const Num = require('./num')
 
-const numbers = value => {
-  return new Num(value)
-}
-
-module.exports = numbers
-module.exports.default = numbers
-
-module.exports.between = Num.between
+module.exports = Num
+module.exports.default = Num
