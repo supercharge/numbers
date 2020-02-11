fe5bfdd6931277d9fa27f9e5e59578c2def1fb89
Marcus Poehls <marcus.poehls@gmail.com>
2020-02-11 05:55:23 +0100

    add tests

--- test/index.js
+++ test/index.js
@@ -8,12 +8,26 @@ const { describe, it } = (exports.lab = Lab.script())
 
 describe('Num', () => {
   it('isInteger', () => {
-    expect(Num(5).isInteger()).to.be.true()
-    expect(Num(1.5).isInteger()).to.be.false()
+    expect(Num.isInteger(5)).to.be.true()
+    expect(Num.isInteger(1.5)).to.be.false()
   })
 
-  it('between', () => {
-    expect(Num.between(2, 5)).to.be.below(6).and.above(1)
-    expect(Num.between(2, 2)).to.equal(2)
+  it('randomInt', () => {
+    expect(Num.randomInt(2, 5)).to.be.in.range(2, 5)
+    expect(Num.randomInt(2, 2)).to.equal(2)
+  })
+
+  it('randomIntBetween', () => {
+    expect(Num.randomIntBetween(2, 5)).to.be.in.range(3, 4)
+    expect(Num.randomIntBetween(2, 4)).to.equal(3)
+    expect(Num.randomIntBetween(2, 2)).to.be.undefined()
+    expect(Num.randomIntBetween(2, 3)).to.be.undefined()
+    expect(Num.randomIntBetween(10, 5)).to.be.undefined()
+  })
+
+  it('diff', () => {
+    expect(Num.diff(2, 5)).to.equal(-3)
+    expect(Num.diff(10, 1, 1, 1)).to.equal(7)
+    expect(Num.diff(10)).to.equal(10)
   })
 })
